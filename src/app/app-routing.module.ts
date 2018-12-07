56ba7efbeb7bc9c7d2ab01870dbb9867b9030a42
Paulo <paulogaspar404@gmail.com>
2018-12-06 23:21:10 -0200

    Made many minor changes to the initial blank project. Changed the package.json file to show my info and info about this project. Formatted some files and removed some comments the way i like as i passed through them. Added a <noscript> tag to the index.html file to show this project needs js if someone tries to run in a browser. Removed the shapes svg file from assets. Added a wildcart that redirects any unwanted page requests to home and added a little text saying hello world with a flexy body centering it.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,10 +4,12 @@ import { Routes, RouterModule } from '@angular/router';
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: './home/home.module#HomePageModule' },
+  { path: '**', redirectTo: 'home'}
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
