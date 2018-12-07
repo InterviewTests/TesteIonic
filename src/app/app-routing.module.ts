0bc1109139e5e90a2b6999814832ae43e3b351e9
Paulo <paulogaspar404@gmail.com>
2018-12-06 23:35:40 -0200

    Added a new page called test. I'll use it for several tests and to see the theme colors. Had to take care with the routing since my wildcart route has to be the last route.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { Routes, RouterModule } from '@angular/router';
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: './home/home.module#HomePageModule' },
+  { path: 'test', loadChildren: './test/test.module#TestPageModule' },
   { path: '**', redirectTo: 'home'}
 ];
 
