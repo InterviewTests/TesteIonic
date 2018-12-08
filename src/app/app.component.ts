85a69012d245a34ece00a2561fc032905324ddf6
Paulo <paulogaspar404@gmail.com>
2018-12-08 18:18:36 -0200

    Added the email and password to the login page and the login and signput button. Added all the validations i could find to the buttons. Styled them and got used to the way ionic and angular work together for styling. It looks better on my device then at the browser.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 
 import { Platform } from '@ionic/angular';
-import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 @Component({
@@ -11,16 +10,12 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 export class AppComponent {
   constructor(
     private platform: Platform,
-    private splashScreen: SplashScreen,
     private statusBar: StatusBar
   ) {
     this.initializeApp();
   }
 
   initializeApp() {
-    this.platform.ready().then(() => {
-      this.statusBar.styleDefault();
-      // this.splashScreen.hide();
-    });
+    this.platform.ready().then(() => this.statusBar.styleDefault());
   }
 }
