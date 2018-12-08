1f02635ad322b806fb534fe2854428a9ce00d25b
Paulo <paulogaspar404@gmail.com>
2018-12-08 18:48:47 -0200

    Revert "Testing the event binding properties of angular. Made a little input select animation that makes the current selected input icon bigger. Used the focus ionic event."
    This reverts commit 66cc6193ae265e0c078ed8fb5788fd5a6e95af18.

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -6,24 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
-  emailSelected:boolean;
-  passwordSelected:boolean;
 
   constructor() { }
 
   ngOnInit() {
-    this.emailSelected = false;
-    this.passwordSelected = false;
-  }
-
-  selectEmail(){
-    this.emailSelected = true;
-    this.passwordSelected = false;
-  }
-  
-  selectPassword(){
-    this.emailSelected = false;
-    this.passwordSelected = true;
   }
 
 }
