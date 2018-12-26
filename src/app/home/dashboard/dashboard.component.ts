dad1277727a45a71e92bbbb8a3ab19c4c659f81e
Paulo <paulogaspar404@gmail.com>
2018-12-26 20:38:19 -0200

    Moved the movies to the dashboard component.

--- src/app/home/dashboard/dashboard.component.ts
+++ src/app/home/dashboard/dashboard.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MoviesService } from '../../api/movies.service';
+import { InfiniteScroll } from '@ionic/angular';
 
 @Component({
   selector: 'app-dashboard',
@@ -6,10 +8,58 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
+  @ViewChild(InfiniteScroll) infiniteScroll: InfiniteScroll;
+  movieLists: any[];
+  showLoader: boolean;
 
-  constructor() { }
+  constructor(
+    private moviesService: MoviesService
+  ) { }
 
   ngOnInit() {
+    this.showLoader = true;
+    this.movieLists = [];
+    // Loading the most popular movies async.
+    this.moviesService.getPopular()
+    .then(response => this.movieLists.push({
+      list: response.results,
+      title: 'Most Popular'
+    }))
+    .catch(() => {});
+    // Loading the best movies async.
+    this.moviesService.getBest()
+    .then(response => this.movieLists.push({
+      list: response.results,
+      title: 'Most Acclaimed'
+    }))
+    .catch(() => {});
+    // Loading the newst movies async.
+    this.moviesService.getNewest()
+    .then(response => this.movieLists.push({
+      list: response.results,
+      title: 'Newest in Brazil'
+    }))
+    .catch(() => {});
   }
 
+  loadMoreMovies (event: CustomEvent) {
+    const { genre, next } = this.moviesService.randomGenre();
+    if (!next) {
+      this.showLoader = false;
+      (<HTMLInputElement>event.target).disabled = true;
+      (<any>event.target).complete();
+    }
+    if (genre) {
+      this.moviesService.getGenre(genre.id)
+      .then(response => {
+        (<any>event.target).complete();
+        this.movieLists.push({list: response.results, title: genre.name});
+      })
+      .catch(() => {
+        (<HTMLInputElement>event.target).disabled = true;
+        this.showLoader = false;
+        (<any>event.target).complete();
+      });
+    }
+  }
 }
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,6 +1,4 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
-import { MoviesService } from '../api/movies.service';
-import { InfiniteScroll } from '@ionic/angular';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { ToastController } from '@ionic/angular';
 
@@ -10,13 +8,9 @@ import { ToastController } from '@ionic/angular';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage implements OnInit {
-  @ViewChild(InfiniteScroll) infiniteScroll: InfiniteScroll;
-  movieLists: any[];
-  showLoader: boolean;
   currentTab = 1;
 
   constructor (
-    private moviesService: MoviesService,
     private toastController: ToastController,
     private activatedRoute: ActivatedRoute
   ) {}
@@ -32,53 +26,10 @@ export class HomePage implements OnInit {
       });
       toast.present();
     }
-    this.showLoader = true;
-    this.movieLists = [];
-    // Loading the most popular movies async.
-    this.moviesService.getPopular()
-    .then(response => this.movieLists.push({
-      list: response.results,
-      title: 'Most Popular'
-    }))
-    .catch(() => {});
-    // Loading the best movies async.
-    this.moviesService.getBest()
-    .then(response => this.movieLists.push({
-      list: response.results,
-      title: 'Most Acclaimed'
-    }))
-    .catch(() => {});
-    // Loading the newst movies async.
-    this.moviesService.getNewest()
-    .then(response => this.movieLists.push({
-      list: response.results,
-      title: 'Newest in Brazil'
-    }))
-    .catch(() => {});
   }
 
   changeTab (tab: number) {
     this.currentTab = tab;
   }
 
-  loadMoreMovies (event: CustomEvent) {
-    const { genre, next } = this.moviesService.randomGenre();
-    if (!next) {
-      this.showLoader = false;
-      (<HTMLInputElement>event.target).disabled = true;
-      (<any>event.target).complete();
-    }
-    if (genre) {
-      this.moviesService.getGenre(genre.id)
-      .then(response => {
-        (<any>event.target).complete();
-        this.movieLists.push({list: response.results, title: genre.name});
-      })
-      .catch(() => {
-        (<HTMLInputElement>event.target).disabled = true;
-        this.showLoader = false;
-        (<any>event.target).complete();
-      });
-    }
-  }
 }
