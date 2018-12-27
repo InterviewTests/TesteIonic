325547e5439b880f9f8dedca41027d72d0bbbd93
Paulo <paulogaspar404@gmail.com>
2018-12-27 21:41:33 -0200

    Update the movie service by adding more fields. These are fields that appear in the movie list response object of the MovieDb Api.

--- src/app/api/movie.ts
+++ src/app/api/movie.ts
@@ -1,4 +1,11 @@
 export interface Movie {
+    id: number;
+    vote_average: number;
+    title: string;
     original_title: string;
     poster_path: string;
+    backdrop_path: string;
+    vote_count: number;
+    overview: string;
+    release_date: string;
 }
