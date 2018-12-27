d56fef1c77e1406d0c0424dd03214fe98875afc9
Paulo <paulogaspar404@gmail.com>
2018-12-27 18:44:39 -0200

    Fixed an import error at the movieList component. Some errors only show up when i stop the app and serve it again. Changed the ion-images to images at the movie list. I lose performance and use too much data by doing this but there is no better way (other than but a black bar at the top of the image to hide the brokenImgIcon, since the ion-image hides the <img> tag inside it's shadow dom and i cannot add a onerror= function to it to hide the broken image link. The api doc says that adding the alt property to the ion-img should hide that broken img icon but it does not. And i dont want the broken img icon to appear. So i changed it to a regular img. I added a little loader animation to the movie poster before it shows up but, since it probably takes a lot or processing power (it is, after all, behind every single image)  i removed it.

--- src/app/home/dashboard/movie-list/movie-list.component.ts
+++ src/app/home/dashboard/movie-list/movie-list.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { Movie } from '../../api/movie';
+import { Movie } from '../../../api/movie';
 
 @Component({
   selector: 'app-movie-list',
