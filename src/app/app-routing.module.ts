0f2d56932434886c1ff4b7acd7b849d4da24dde4
Paulo <paulogaspar404@gmail.com>
2018-12-28 02:49:43 -0200

    Undoing last commit (manually to see if i get the gist of ionic and angular 100%) because the movie my list should not be a page but instead a component.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,8 +7,7 @@ const routes: Routes = [
   { path: 'test', loadChildren: './test/test.module#TestPageModule' },
   { path: 'auth', loadChildren: './auth/auth.module#AuthPageModule' },
   { path: 'splashAnimation', loadChildren: './splash-animation/splash-animation.module#SplashAnimationPageModule' },
-  { path: '**', redirectTo: 'home'},
-  { path: 'myList', loadChildren: './home/my-list/my-list.module#MyListPageModule' }
+  { path: '**', redirectTo: 'home'}
 ];
 
 @NgModule({
