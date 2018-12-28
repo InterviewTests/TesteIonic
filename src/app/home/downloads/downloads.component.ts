a9498baf875abdeec77100131ebeebb81d35abae
Paulo <paulogaspar404@gmail.com>
2018-12-28 04:19:14 -0200

    Downloaded movies are sent to the downloaded movies page and show a  loading animation for 15 seconds starting from the exact moment they are downloaded since it is stored locally. Then, after that time, an option to delete the movie appears at the donwloads component as well.

--- src/app/home/downloads/downloads.component.ts
+++ src/app/home/downloads/downloads.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Movie } from 'src/app/api/movie';
+import { ToastController, LoadingController } from '@ionic/angular';
 
 @Component({
   selector: 'app-downloads',
@@ -8,10 +9,34 @@ import { Movie } from 'src/app/api/movie';
 })
 export class DownloadsComponent implements OnInit {
   @Input() public downloads: Movie[];
+  now: number;
 
-  constructor() { }
+  constructor(
+    private loadingController: LoadingController,
+    private toastController: ToastController
+  ) { }
 
   ngOnInit() {
+    setInterval(() => this.now = Date.now(), 1000);
+  }
+
+  async delete(id: Number) {
+    const loading = await this.loadingController.create({
+      keyboardClose: true,
+      translucent: true
+    });
+    const toastDef = {
+      message: 'Movie Deleted!',
+      color: 'success',
+      showCloseButton: false,
+      duration: 2000
+    };
+    await loading.present();
+    const index = this.downloads.findIndex(d => d.id === id);
+    this.downloads.splice(index, 1);
+    const toast = await this.toastController.create(toastDef);
+    toast.present();
+    await loading.dismiss();
   }
 
 }
