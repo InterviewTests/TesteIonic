ba9e6872011a0278bb1bb28460a9168cfe8f6827
Paulo <paulogaspar404@gmail.com>
2018-12-30 08:24:54 -0200

    Fixed the settings component spec.

--- src/app/home/settings/settings.component.spec.ts
+++ src/app/home/settings/settings.component.spec.ts
@@ -1,15 +1,35 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { SettingsComponent } from './settings.component';
+import { ToastController, LoadingController } from '@ionic/angular';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { FIREBASE_CREDENTIALS } from 'src/app/firebase.credentials';
+import { IonicStorageModule } from '@ionic/storage';
+import { Router } from '@angular/router';
 
 describe('SettingsComponent', () => {
   let component: SettingsComponent;
   let fixture: ComponentFixture<SettingsComponent>;
+  let routerSpy: any;
 
   beforeEach(async(() => {
+    routerSpy = jasmine.createSpyObj('Router', ['navigate']);
     TestBed.configureTestingModule({
       declarations: [ SettingsComponent ],
-      schemas: [ CUSTOM_ELEMENTS_SCHEMA ]
+      schemas: [ CUSTOM_ELEMENTS_SCHEMA ],
+      providers: [
+        ToastController,
+        LoadingController,
+        { provide: Router, useValue: routerSpy },
+      ],
+      imports: [
+        AngularFireModule.initializeApp(FIREBASE_CREDENTIALS),
+        AngularFireAuthModule,
+        AngularFirestoreModule,
+        IonicStorageModule.forRoot()
+      ]
     })
     .compileComponents();
   }));
