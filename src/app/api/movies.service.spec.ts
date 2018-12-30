85c613e4efc2e26c46cfca75696aed8c6b6ab984
Paulo <paulogaspar404@gmail.com>
2018-12-30 06:42:36 -0200

    While learning Karma and Jasmine i'm findng some errors i did not find on angular,ionic or jasmine doc. For example this one. I had problems with this service since it started out with assertion errors. Fixed the movieService created spec by adding the IonicModule and Storage imports as well as the firestore imports. Also asked the testBed to compile the components. Added a Test movie list spec that always fails since i did not write it.

--- src/app/api/movies.service.spec.ts
+++ src/app/api/movies.service.spec.ts
@@ -1,12 +1,42 @@
 import { TestBed } from '@angular/core/testing';
-
 import { MoviesService } from './movies.service';
+import { MOVIE_DB_API_KEY } from '../moviedb.credentials';
+import { Movie } from './movie';
+import { User } from './user';
+import { genres } from './genres';
+import { Storage } from '@ionic/storage';
+import { IonicStorageModule } from '@ionic/storage';
+
+import {
+  AngularFirestore,
+  AngularFirestoreDocument,
+  AngularFirestoreCollection
+} from '@angular/fire/firestore';
+import { IonicModule } from '@ionic/angular';
 
 describe('MoviesService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        IonicModule.forRoot(),
+        IonicStorageModule.forRoot()
+      ],
+      providers: [
+        MoviesService,
+        AngularFirestore,
+        AngularFirestoreDocument,
+        AngularFirestoreCollection
+      ]
+    }).compileComponents();
+  });
 
   it('should be created', () => {
     const service: MoviesService = TestBed.get(MoviesService);
     expect(service).toBeTruthy();
   });
+
+  it('get movie list', async () => {
+    fail('Potato');
+  });
+
 });
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -9,7 +9,10 @@ import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
 
-  let statusBarSpy, splashScreenSpy, platformReadySpy, platformSpy;
+  let statusBarSpy: any,
+      splashScreenSpy: any,
+      platformReadySpy: any,
+      platformSpy: any;
 
   beforeEach(async(() => {
     statusBarSpy = jasmine.createSpyObj('StatusBar', ['styleDefault']);
