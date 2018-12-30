7edd0f3a47f2ef1e30d9d0dec1ce0eb8eb9eeacc
Paulo <paulogaspar404@gmail.com>
2018-12-30 07:41:32 -0200

    Thanks to all the work and stress i had at the login spec fix, the registration spec was way too easy. Only had to copy the setup i made for the login component.

--- src/app/auth/register/register.component.spec.ts
+++ src/app/auth/register/register.component.spec.ts
@@ -1,14 +1,40 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { IonicStorageModule } from '@ionic/storage';
+import { IonicModule } from '@ionic/angular';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { FIREBASE_CREDENTIALS } from 'src/app/firebase.credentials';
+import { Router, ActivatedRoute } from '@angular/router';
+import { ActivatedRouteStub } from 'src/app/testing/ActivatedRouteStub';
 import { RegisterComponent } from './register.component';
 
 describe('RegisterComponent', () => {
   let component: RegisterComponent;
   let fixture: ComponentFixture<RegisterComponent>;
 
+  let routerSpy: any;
+  let activatedRouteStub: ActivatedRouteStub;
+
   beforeEach(async(() => {
+    routerSpy = jasmine.createSpyObj('Router', ['navigate']);
+    activatedRouteStub = new ActivatedRouteStub();
+
     TestBed.configureTestingModule({
-      declarations: [ RegisterComponent ]
+      declarations: [ RegisterComponent ],
+      providers: [
+        { provide: Router, useValue: routerSpy },
+        { provide: ActivatedRoute, useValue: activatedRouteStub }
+      ],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
+      imports: [
+        AngularFireModule.initializeApp(FIREBASE_CREDENTIALS),
+        AngularFireAuthModule,
+        AngularFirestoreModule,
+        IonicModule.forRoot(),
+        IonicStorageModule.forRoot()
+      ]
     })
     .compileComponents();
   }));
