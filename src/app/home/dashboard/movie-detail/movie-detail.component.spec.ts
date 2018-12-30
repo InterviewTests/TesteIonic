7b29c509d8aff1efa504ef7e7aba37a848472aa7
Paulo <paulogaspar404@gmail.com>
2018-12-30 08:15:48 -0200

    fixed movie details and mocked the @input lists.

--- src/app/home/dashboard/movie-detail/movie-detail.component.spec.ts
+++ src/app/home/dashboard/movie-detail/movie-detail.component.spec.ts
@@ -2,6 +2,12 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { MovieDetailComponent } from './movie-detail.component';
 import { SocialSharing } from '@ionic-native/social-sharing/ngx';
+import { ToastController, LoadingController } from '@ionic/angular';
+import { IonicStorageModule } from '@ionic/storage';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { FIREBASE_CREDENTIALS } from 'src/app/firebase.credentials';
 
 describe('MovieDetailComponent', () => {
   let component: MovieDetailComponent;
@@ -13,8 +19,16 @@ describe('MovieDetailComponent', () => {
     TestBed.configureTestingModule({
       declarations: [ MovieDetailComponent ],
       schemas: [ CUSTOM_ELEMENTS_SCHEMA ],
+      imports: [
+        AngularFireModule.initializeApp(FIREBASE_CREDENTIALS),
+        AngularFireAuthModule,
+        AngularFirestoreModule,
+        IonicStorageModule.forRoot()
+      ],
       providers: [
-        {provide: SocialSharing, useValue: socialSharingSpy}
+        {provide: SocialSharing, useValue: socialSharingSpy},
+        ToastController,
+        LoadingController
       ]
     })
     .compileComponents();
@@ -23,6 +37,17 @@ describe('MovieDetailComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(MovieDetailComponent);
     component = fixture.componentInstance;
+    component.favorites = {
+      title: 'Favorites',
+      list: []
+    };
+    component.myList = {
+      title: 'My List',
+      list: []
+    };
+
+    component.downloads = [];
+
     fixture.detectChanges();
   });
 
