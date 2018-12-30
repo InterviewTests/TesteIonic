6c236aa93a937b2b0ab10e70ba5b4ae71899dc4b
Paulo <paulogaspar404@gmail.com>
2018-12-30 08:22:18 -0200

    Fixed all the home spec complicated dependencies.

--- src/app/home/home.page.spec.ts
+++ src/app/home/home.page.spec.ts
@@ -2,18 +2,33 @@ import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { ToastController, LoadingController } from '@ionic/angular';
 import { HomePage } from './home.page';
+import { IonicStorageModule } from '@ionic/storage';
+import { ActivatedRoute } from '@angular/router';
+import { ActivatedRouteStub } from 'src/app/testing/ActivatedRouteStub';
+import { Platform } from '@ionic/angular';
 
 describe('HomePage', () => {
   let component: HomePage;
   let fixture: ComponentFixture<HomePage>;
+  let activatedRouteStub: ActivatedRouteStub;
+  let platformReadySpy: any;
+  let platformSpy: any;
 
   beforeEach(async(() => {
+    platformReadySpy = Promise.resolve();
+    platformSpy = jasmine.createSpyObj('Platform', { ready: platformReadySpy });
+    activatedRouteStub = new ActivatedRouteStub();
     TestBed.configureTestingModule({
       declarations: [ HomePage ],
       schemas: [CUSTOM_ELEMENTS_SCHEMA],
       providers: [
         ToastController,
-        LoadingController
+        LoadingController,
+        { provide: Platform, useValue: platformSpy },
+        { provide: ActivatedRoute, useValue: activatedRouteStub }
+      ],
+      imports: [
+        IonicStorageModule.forRoot()
       ]
     })
       .compileComponents();
