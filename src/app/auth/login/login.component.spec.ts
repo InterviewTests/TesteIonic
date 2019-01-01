bf7915204821940773013b2561ee80442db3d940
Paulo <paulogaspar404@gmail.com>
2019-01-01 01:51:47 -0200

    Had to remove last commit from github with a hard reset and a forced push since i comitted a password i actually use.

--- src/app/auth/login/login.component.spec.ts
+++ src/app/auth/login/login.component.spec.ts
@@ -1,5 +1,6 @@
-import { async, ComponentFixture, TestBed, fakeAsync } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { LoginComponent } from './login.component';
+import { AuthPage } from '../auth.page';
 import { FormComponent } from 'src/app/utils/form/form.component';
 import { IonicStorageModule } from '@ionic/storage';
 import { IonicModule } from '@ionic/angular';
@@ -11,7 +12,9 @@ import { Router, ActivatedRoute } from '@angular/router';
 import { ActivatedRouteStub } from 'src/app/testing/ActivatedRouteStub';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterTestingModule } from '@angular/router/testing';
-import {Location} from '@angular/common';
+import { Location } from '@angular/common';
+import { RegisterComponent } from '../register/register.component';
+import { routes } from '../auth.routes';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
@@ -23,7 +26,7 @@ describe('LoginComponent', () => {
   beforeEach(async(() => {
     activatedRouteStub = new ActivatedRouteStub();
     TestBed.configureTestingModule({
-      declarations: [ LoginComponent, FormComponent ],
+      declarations: [ AuthPage, LoginComponent, RegisterComponent, FormComponent ],
       providers: [
         { provide: ActivatedRoute, useValue: activatedRouteStub }
       ],
@@ -31,7 +34,7 @@ describe('LoginComponent', () => {
         AngularFireModule.initializeApp(FIREBASE_CREDENTIALS),
         AngularFireAuthModule,
         AngularFirestoreModule,
-        RouterTestingModule.withRoutes([]),
+        RouterTestingModule.withRoutes(routes),
         FormsModule,
         ReactiveFormsModule,
         IonicModule.forRoot(),
@@ -47,6 +50,7 @@ describe('LoginComponent', () => {
     fixture.detectChanges();
     location = TestBed.get(Location);
     router = TestBed.get(Router);
+    router.initialNavigation();
     spyOn(router, 'navigate');
   });
 
@@ -78,11 +82,35 @@ describe('LoginComponent', () => {
     expect(loginEmail.value).toEqual(email);
   });
 
-  it('Go to registration page', fakeAsync(() => {
+  it('Go to registration page', () => {
     const html = fixture.nativeElement;
     html.querySelector('.primary').click();
+    fixture.detectChanges();
     fixture.whenStable()
-    .then(() => expect(location.path()).toBe('/auth/register'))
+    .then(() => expect(location.path()).toBe('/register'))
     .catch(() => fail('Failed at click'));
-  }));
+  });
+
+  it('Bad login attempt', async () => {
+    const html = fixture.nativeElement;
+    html.querySelector('#loginEmail').value = 'error@error.com';
+    html.querySelector('#loginPassword').value = '12345678';
+    html.querySelector('.primary').click();
+    fixture.detectChanges();
+    return fixture.whenStable()
+    .then(() => expect(location.path()).toBe('/login'))
+    .catch(() => fail('Could not validate bad login attempt.'));
+  });
+
+  it('Ok login attempt', async () => {
+    const html = fixture.nativeElement;
+    // html.querySelector('#loginEmail').value = '';
+    // html.querySelector('#loginPassword').value = '';
+    html.querySelector('.primary').click();
+    fixture.detectChanges();
+    return fixture.whenStable()
+    .then(() => expect(location.path()).toBe('/home'))
+    .catch(() => fail('Could not validate bad login attempt.'));
+  });
+
 });
