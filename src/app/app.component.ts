f41661fb9d9a0255c66e8a5b502f512d759a587c
Daniel F <daniielf.13@gmail.com>
2019-01-03 16:35:40 -0200

    Side Menu Prototipo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,9 +6,27 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 @Component({
   selector: 'app-root',
-  templateUrl: 'app.component.html'
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.page.scss'],
+
 })
 export class AppComponent {
+
+  private appMenuItems = [
+    { label: 'Início' },
+    { label: 'Categorias' },
+    { label: 'Sair' }
+  ];
+
+  private categoriesItems = [
+    { label: 'Ação' },
+    { label: 'Aventura' },
+    { label: 'Comédia' },
+    { label: 'Drama' },
+    { label: 'Suspense' },
+    { label: 'Terror' }
+  ];
+
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
